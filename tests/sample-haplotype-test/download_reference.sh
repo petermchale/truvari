45c8755ebac315a9e16a055c0264624ef321e6e3
petermchale <peter.thomas.mchale@gmail.com>
2020-04-27 17:29:59 -0600

    reference

--- tests/sample-haplotype-test/download_reference.sh
+++ tests/sample-haplotype-test/download_reference.sh
@@ -1,1 +1,17 @@
-/scratch/ucgd/lustre-work/quinlan/u6018199/chaisson_2019/download_reference.sh
+#set -o errexit
+#set -o pipefail
+#set -o nounset
+#set -o xtrace
+
+#wget --no-clobber ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa
+#wget --no-clobber ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa.fai
+#wget --no-clobber ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa.alt 
+#wget --no-clobber ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa.amb
+#wget --no-clobber ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa.ann
+#wget --no-clobber ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa.bwt
+#wget --no-clobber ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa.pac
+#wget --no-clobber ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa.sa
+#wget --no-clobber ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.dict
+
+./chromosome_sizes.sh GRCh38_full_analysis_set_plus_decoy_hla.fa > GRCh38_full_analysis_set_plus_decoy_hla.genome
+
